3b6b7afeb3241871ca9cd16ea673aa4dd61b211b
Your Name <y.y.oleksander@gmail.com>
2024-03-06 13:30:48 +0100

    test

--- src/main.tsx
+++ src/main.tsx
@@ -5,19 +5,23 @@ import "./index.css";
 import setLevelEvent from "./engine/events/setLevelEvent.ts";
 import keyboardEvents from "./engine/events/keyboardEvents.ts";
 
-import handleSwipeEvent from "./engine/events/handleSwipeEvent.ts";
+// import handleSwipeEvent from "./engine/events/handleSwipeEvent.ts";
 // import changeDirectionEvent from "./engine/events/changeDirectionEvent.ts";
 // import swipeEvent from "./engine/events/swipeEvent.ts";
 
 setLevelEvent(1);
 // document.addEventListener("keydown", changeDirectionEvent);
 document.addEventListener("keydown", keyboardEvents);
-document.addEventListener("touchstart", handleSwipeEvent);
-document.addEventListener("touchmove", handleSwipeEvent);
-// document.addEventListener("touchstart", swipeEvent);
-// const gameElement = document.getElementById("root");
 
+// document.addEventListener("touchstart", swipe);
+// const gameElement = document.getElementById("root");
+// const playGround = document.querySelector("#root");
 // console.log(gameElement);
+// if (playGround) {
+// document.addEventListener("touchstart", handleSwipeEvent);
+// document.addEventListener("touchmove", handleSwipeEvent);
+// }
+// console.log(playGround);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
