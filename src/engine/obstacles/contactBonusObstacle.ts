9424397ee54a380ab52965912ccc451f7bfeed42
MVprogramm <michael.valiahmetov@gmail.com>
2024-04-03 23:29:14 +0300

    fix bug with contact obstacle and bonus

--- src/engine/obstacles/contactBonusObstacle.ts
+++ src/engine/obstacles/contactBonusObstacle.ts
@@ -4,6 +4,7 @@
  */
 import { obstacleContactProps } from "../../types/obstacle";
 import { getBonusCoord } from "../bonuses/bonus";
+import { getBonusAvailability } from "../bonuses/bonusAvailableState";
 import { getBonusCatchingStatus } from "../bonuses/bonusCatchingState";
 import obstacleBounce from "./obstacleBounce";
 /**
@@ -14,7 +15,7 @@ import obstacleBounce from "./obstacleBounce";
  */
 function contactBonusObstacle(props: obstacleContactProps): number {
   const { i, step } = props;
-  if (!getBonusCatchingStatus().isBonusCaught)
+  if (!getBonusCatchingStatus().isBonusCaught && getBonusAvailability())
     step[i] = obstacleBounce({ ...props, cell: getBonusCoord() });
 
   return step[i];
