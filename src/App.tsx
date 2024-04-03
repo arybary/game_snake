4e38a21428f054f1a543e0df5656b220c7bd8bf1
MVprogramm <michael.valiahmetov@gmail.com>
2024-04-03 15:17:46 +0300

    fix bug in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import Wrapper from "./Components/Wrapper";
 import Game from "./Components/Game";
-import { OrbitControls } from "@react-three/drei";
+// import { OrbitControls } from "@react-three/drei";
 
 function App() {
   return (
