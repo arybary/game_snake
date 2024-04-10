b2aeca7f44c6225489d5adc35aea49e959371804
MVprogramm <michael.valiahmetov@gmail.com>
2024-04-10 23:30:17 +0300

    10.04

--- src/engine/protocol/foodEaten.ts
+++ src/engine/protocol/foodEaten.ts
@@ -7,7 +7,6 @@ import { getStopsGrowing } from "../bonuses/bonusSnakeStopsGrowing";
 import { getFoodScores } from "../food/food";
 import setFood from "../food/setFood";
 import { setScores } from "../scores/scores";
-import moveSnake from "../snake/moveSnake";
 import * as SNAKE from "../snake/snake";
 /**
  * Выводит новую еду, задает вознаграждение, добавляет длину змейке, если можно
@@ -16,7 +15,6 @@ function foodEaten() {
   setFood(1);
   setScores(getDoubleScoresFood() ? getFoodScores() * 2 : getFoodScores());
   if (!getStopsGrowing()) SNAKE.addSnakeBodyCoord([]);
-  moveSnake();
 }
 
 export default foodEaten;
