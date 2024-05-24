3a95259e200b40ec085ed0f4cf8c92b400900396
MVprogramm <michael.valiahmetov@gmail.com>
2024-05-24 20:18:34 +0300

    fixed bug with pause & modal

--- src/Components/Wrapper/Wrapper.tsx
+++ src/Components/Wrapper/Wrapper.tsx
@@ -5,8 +5,10 @@ import swipeDirectionEvent from "../../engine/events/swipeDirectionEvent";
 import { setTouch } from "../../engine/events/touchEvent";
 import protocolExecutor from "../../engine/protocol/protocolExecutor";
 import GameButtons from "../GameButtons/GameButtons";
+import { useMenuStore } from "../../store/menuStore";
 
 function Wrapper({ children }: { children: ReactNode }) {
+  const { isVisible } = useMenuStore();
   const startTouch = (e: React.TouchEvent<HTMLDivElement>) => {
     const touches = e.changedTouches;
     setTouch("start", touches[0].clientX, touches[0].clientY);
@@ -14,7 +16,7 @@ function Wrapper({ children }: { children: ReactNode }) {
   const endTouch = (e: React.TouchEvent<HTMLDivElement>) => {
     const touches = e.changedTouches;
     setTouch("end", touches[0].clientX, touches[0].clientY);
-    protocolExecutor(swipeDirectionEvent());
+    if (!isVisible) protocolExecutor(swipeDirectionEvent());
   };
   return (
     <div className="wrapper" onTouchStart={startTouch} onTouchEnd={endTouch}>
