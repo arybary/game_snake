3e615d07b9d37d0c3b1f279218a6a0c8435d907c
MVprogramm <michael.valiahmetov@gmail.com>
2024-05-28 08:59:20 +0300

    fixed bug with pausa event

--- src/Components/Menu/Menu.tsx
+++ src/Components/Menu/Menu.tsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { useMenuStore } from "../../store/menuStore";
+import { useMenuStore, usePauseStore } from "../../store/menuStore";
 import "./Menu.css";
+import { swapPause } from "../../engine/events/pauseEvent";
 
 const Menu: React.FC = () => {
   const { toggleModal, titleMenu } = useMenuStore();
+  const { togglePause } = usePauseStore();
   return (
     <div
       className="menu-game"
       onClick={() => {
         if (titleMenu.indexOf("Game over") !== -1) location.reload();
-        else toggleModal();
+        else if (titleMenu === "Pause") {
+          togglePause();
+          swapPause();
+          toggleModal();
+        } else toggleModal();
       }}
     >
       <h2>{titleMenu}</h2>
